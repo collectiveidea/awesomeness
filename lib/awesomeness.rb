37eaed4b899321f9b115c54eba200371e7e99d09
Daniel Morrison <daniel@collectiveidea.com>
2007-04-25 13:48:51 +0000

    Added better unicode characters as default in truncate and excerpt methods

--- lib/awesomeness.rb
+++ lib/awesomeness.rb
@@ -4,3 +4,23 @@ Dir[File.dirname(__FILE__) + "/awesomeness/**/*.rb"].each {|f| require f }
 if RAILS_ENV == 'test'
   Dir[File.dirname(__FILE__) + "/test/**/*.rb"].each {|f| require f }
 end
+
+
+module ActionView
+  module Helpers
+    module TextHelper
+      
+      def truncate_with_better_default(text, length = 30, truncate_string = "…")
+        truncate_without_better_default(text, length, truncate_string)
+      end
+      
+      alias_method_chain :truncate, :better_default
+      
+      def excerpt_with_better_default(text, phrase, radius = 100, excerpt_string = "…")
+        excerpt_without_better_default(text, phrase, radius, excerpt_string)
+      end
+      
+      alias_method_chain :excerpt, :better_default
+    end
+  end
+end
