a0e4d689478142bbac06a9149e7e8709db5a6965
Daniel Morrison <daniel@collectiveidea.com>
2007-06-26 15:06:45 +0000

    Added Widon't support to awesomeness

--- lib/awesomeness.rb
+++ lib/awesomeness.rb
@@ -21,6 +21,31 @@ module ActionView
       end
       
       alias_method_chain :excerpt, :better_default
+      
+      # Widon't
+      def widont(text)
+        text.widont
+      end
+      
+      def textilize_with_widont(text)
+        textilize_without_widont(text.widont)
+      end
+      alias_method_chain :textilize, :widont
+      
+      def textilize_without_paragraph_with_widont(text)
+        textilize_without_paragraph_without_widont(text.widont)
+      end
+      alias_method_chain :textilize_without_paragraph, :widont
     end
+  end
+end
+
+class String
+  # Widon't
+  # Based on the original by Shaun Inman: http://shauninman.com/archive/2006/08/22/widont_wordpress_plugin
+  # And the Ruby versions here: http://mucur.name/posts/widon-t-helper-for-rails-2
+  # This version replaces &nbsp; with a unicode non-breaking space (option-space on a Mac)
+  def widont
+    self.gsub(/([^\s])\s+([^\s]+)(\s*)$/, '\1 \2\3')
   end
 end
