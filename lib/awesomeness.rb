665260ffe52dfbbb804015de64e0a70374a1b858
Daniel Morrison <daniel@collectiveidea.com>
2007-06-26 20:05:59 +0000

    Added Object#tap

--- lib/awesomeness.rb
+++ lib/awesomeness.rb
@@ -53,3 +53,12 @@ class String
     self.gsub(/([^\s])\s+([^\s]+)(\s*)$/, '\1 \2\3')
   end
 end
+
+class Object
+  # See http://moonbase.rydia.net/mental/blog/programming/eavesdropping-on-expressions
+  # Will be added in Ruby 1.9
+  def tap
+    yield self
+    self
+  end if RUBY_VERSION < '1.9'
+end
