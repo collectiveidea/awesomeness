669ac6676d8206309e288b516ce78c6e3c9adf69
Brandon Keepers <brandon@collectiveidea.com>
2007-12-06 21:21:13 +0000

    move backup into rake

--- recipies/backup.rb
+++ recipies/backup.rb
@@ -1,49 +1,46 @@
 before "deploy:migrate", "backup:create"
 
 _cset(:backup_path) { "#{shared_path}/backups" }
-_cset :backup_name, Time.now.utc.strftime("%Y%m%d%H%M%S")
 
 namespace :backup do
 
-  def backup_file(options = {})
-    options = {:path => true}.merge(options)
-    "#{options[:path] ? backup_path + '/' : ''}#{backup_name}.tar.gz"
-  end
-
-  def latest_backup
-    last = capture("ls -x #{backup_path}").split.sort.last
-    last.gsub(/\.tar\.gz$/, '') if last
-  end
+  # def backup_file(options = {})
+  #   options = {:path => true}.merge(options)
+  #   "#{options[:path] ? backup_path + '/' : ''}#{backup_name}.tar.gz"
+  # end
+  # 
+  # def latest_backup
+  #   last = capture("ls -x #{backup_path}").split.sort.last
+  #   last.gsub(/\.tar\.gz$/, '') if last
+  # end
+  # 
+  # task :default, :roles => :db, :only => {:primary => true} do
+  #   create
+  #   retrieve
+  # end
   
-  task :default, :roles => :db, :only => {:primary => true} do
-    create
-    retrieve
-  end
-
-  desc "Backup the remote production database"
-  task :create, :roles => :db, :only => {:primary => true} do
-    backup_dir = "#{backup_path}/#{backup_name}"
+  task :remote_to_local, :roles => :db, :only => {:primary => true} do
     rails_env = fetch(:rails_env, "production")
-    
-    remove_command = "rm -rf #{backup_dir}; true"
-    on_rollback { run remove_command }
-    
-    run "mkdir -p #{backup_dir}"
-    run "cd #{current_path}; rake db:fixtures:dump RAILS_ENV=#{rails_env} FIXTURES_DIR=#{backup_dir}"
-    run "cd #{current_path}; rake db:schema:dump RAILS_ENV=#{rails_env}"
-    run "cp #{current_path}/db/schema.rb #{backup_dir}/"
-    run "tar -C #{backup_path} -czf #{backup_file} #{backup_name}"
-    run remove_command
+    run "cd #{current_path}; rake backup:create RAILS_ENV=#{rails_env} BACKUP_DIR=#{backup_path}"
+    latest = capture "cd #{current_path}; rake -s backup:latest".strip
+    run "tar -C #{backup_path} -czf #{backup_path}/#{latest}.tar.gz #{latest}"
+    get "#{backup_path}/#{latest}", "backups/#{latest}.tar.gz"
+    `tar -C -zxf backups/#{latest}.tar.gz`
   end
-  
-  task :retrieve, :roles => :db, :only => {:primary => true} do
-    get backup_file, "backups/#{backup_file(:path => false)}"
-  end
-  
-  task :retrieve_latest, :roles => :db, :only => {:primary => true} do
-    set :backup_name, latest_backup
-    fetch
-  end
-  
-  
+
+  # desc "Backup the remote production database"
+  # task :create, :roles => :db, :only => {:primary => true} do
+  #   backup_dir = "#{backup_path}/#{backup_name}"
+  #   rails_env = fetch(:rails_env, "production")
+  #   
+  #   remove_command = "rm -rf #{backup_dir}; true"
+  #   on_rollback { run remove_command }
+  #   
+  #   run "mkdir -p #{backup_dir}"
+  #   run "cd #{current_path}; rake db:fixtures:dump RAILS_ENV=#{rails_env} FIXTURES_DIR=#{backup_dir}"
+  #   run "cd #{current_path}; rake db:schema:dump RAILS_ENV=#{rails_env}"
+  #   run "cp #{current_path}/db/schema.rb #{backup_dir}/"
+  #   run "tar -C #{backup_path} -czf #{backup_file} #{backup_name}"
+  #   run remove_command
+  # end
 end
--- tasks/backup.rake
+++ tasks/backup.rake
@@ -1,15 +1,27 @@
 namespace :backup do
-  def latest_version
-    last = Dir['backups/*.tar.gz'].sort.last
-    File.basename(last, '.tar.gz') if last
+  
+  task :directory do
+    ENV['BACKUP_DIR'] ||= 'backups'
   end
   
-  task :environment do
-    backup = "backups/#{ENV['VERSION'] || latest_version}"
-    `tar -C backups -zxf #{backup}.tar.gz` unless File.exist?(backup)
-    ENV['FIXTURES_DIR'] = "#{backup}"
+  task :latest => :directory do
+    last = Dir["#{ENV['BACKUP_DIR']}/*/"].sort.last
+    puts ENV['VERSION'] ||= File.basename(last) if last
+  end
+  
+  task :environment => :directory do
+    ENV['VERSION'] ||= Time.now.utc.strftime("%Y%m%d%H%M%S")
+    backup = "#{ENV['BACKUP_DIR']}/#{ENV['VERSION']}"
+    FileUtils.mkdir_p backup
+    ENV['FIXTURES_DIR'] = backup
     ENV['SCHEMA'] = "#{backup}/schema.rb"
   end
   
-  task :restore => [:environment, 'db:schema:load', 'db:fixtures:load']
+  task :schema => :environment do
+    `cp db/schema.rb #{ENV['FIXTURES_DIR']}/`
+  end
+  
+  task :create => [:environment, 'db:fixtures:dump', 'db:schema:dump']
+  
+  task :restore => [:latest, :environment, 'db:schema:load', 'db:fixtures:load']
 end
