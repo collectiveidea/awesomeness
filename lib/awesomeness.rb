a50495fef3b41c27d5880c9c3b1caecc181879f4
Brandon Keepers <brandon@collectiveidea.com>
2008-06-20 12:43:18 -0400

    added #log_to

--- lib/awesomeness.rb
+++ lib/awesomeness.rb
@@ -3,3 +3,12 @@ Dir[File.dirname(__FILE__) + "/awesomeness/**/*.rb"].each {|f| require f }
 if RAILS_ENV == 'test'
   Dir[File.dirname(__FILE__) + "/test/**/*.rb"].each {|f| require f }
 end
+
+# From: http://weblog.jamisbuck.org/2007/1/31/more-on-watching-activerecord
+# Easy way to view logs in script/console.
+# simply type: log_to(STDOUT)
+# and all of your active record queries will show up inline.
+def log_to(stream = STDOUT)
+  ActiveRecord::Base.logger = Logger.new(stream)
+  ActiveRecord::Base.clear_active_connections!
+end
