9d1a4eba8824de2ef50dabd60fe88490bd28fbe8
Brandon Keepers <brandon@collectiveidea.com>
2008-06-19 23:04:19 -0400

    refactoring Enum#/, making it an alias for #chunk, adding more tests and adding an option to pad last chunk.

--- lib/awesomeness/core_ext/enumerable.rb
+++ lib/awesomeness/core_ext/enumerable.rb
@@ -1,10 +1,12 @@
 module Enumerable
 
   # Divide into groups
-  def /(num)
-    returning [] do |result|
-      each_slice((size.to_f / num).ceil) {|a| result << a }
+  def chunk(number_of_chunks, padding = false)
+    returning enum_for(:each_slice, (size.to_f / number_of_chunks).ceil).to_a do |result|
+      result << [] while result.length < number_of_chunks
+      result.last.pad!(result.first.length, padding) unless padding == false
     end
   end
+  alias / chunk
   
 end
--- test/core_ext/enumerable_test.rb
+++ test/core_ext/enumerable_test.rb
@@ -1,13 +1,39 @@
 require File.dirname(__FILE__) + '/../test_helper'
+require 'awesomeness/core_ext/array'
 require 'awesomeness/core_ext/enumerable'
 
 class EnumerableTest < Test::Unit::TestCase
   
-  def test_divide
+  def test_chunk_evenly
+    assert_equal [[1], [2], [3]], [1, 2, 3].chunk(3)
+  end
+
+  def test_divide_evenly
+    assert_equal [[1], [2], [3]], [1, 2, 3] / 3
+    assert_equal [[1, 2], [3, 4], [5, 6]], [1, 2, 3, 4, 5, 6].chunk(3)
     assert_equal [[1,2,3,4,5],[6,7,8,9,10]], ((1..10).to_a / 2)
-    assert_equal [[1,2,3,4],[5,6,7,8],[9,10]], ((1..10).to_a / 3)
+  end
+
+  def test_chunk_unevenly
+    assert_equal [[1, 2], [3]], [1, 2, 3].chunk(2)
     assert_equal [[1,2,3,4,5,6,7],[8,9,10,11,12,13]], ((1..13).to_a / 2)
-    # assert_equal([[[:a, 1], [:b, 2]], [[:c, 3]]], {:a => 1, :b => 2, :c => 3} / 2)
+    assert_equal [[1,2,3,4],[5,6,7,8],[9,10]], ((1..10).to_a / 3)
+  end
+
+  def test_chunk_fills_with_empty_arrays
+    assert_equal [[1], [], []], [1].chunk(3)
+  end
+  
+  def test_chunking_does_not_modify_original
+    array = [1, 2, 3]
+    assert_equal 3, array.length
+    assert_equal [[1], [2], [3]], array.chunk(3)
+    assert_equal 3, array.length
+  end
+  
+  def test_chunk_unevenly_with_padding
+    assert_equal [[1, 2], [3, nil]], [1, 2, 3].chunk(2, nil)
+    assert_equal [[1,2,3,4,5,6,7],[8,9,10,11,12,13, 'foo']], ((1..13).to_a.chunk(2, 'foo'))
   end
   
 end
